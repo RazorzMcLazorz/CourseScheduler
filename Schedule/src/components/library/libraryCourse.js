5c0e71a0b9ec34dcd2a439e2f91695dd429e578c
NathanielYoung <young.nathaniel95@gmail.com>
2018-09-17 13:32:34 -0600

    icons

--- Schedule/src/components/library/libraryCourse.js
+++ Schedule/src/components/library/libraryCourse.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className="library-course__title">Problem Solving</label>
+                { Icon("fas fa-check", "library-couse__icon") }
+                { Icon("fas fa-plus-circle") }
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>
