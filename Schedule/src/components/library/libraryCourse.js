2083b612b537d7d39ce80b03c18e082dbd1243be
NathanielYoung <young.nathaniel95@gmail.com>
2018-09-17 13:19:58 -0600

    grid layout

---
+++ Schedule/src/components/library/libraryCourse.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LibraryCourse;
